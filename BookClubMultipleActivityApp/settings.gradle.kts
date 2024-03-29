08032a35873540eefc42b751fac452550974b33a
Maria Biserica <maria.biserica@student.unitbv.ro>
2024-03-30 01:18:21 +0200

    Created 'Empty Views Activity' app and added the RecyclerView dependency to build.gradle (Module: app) file.

---
+++ BookClubMultipleActivityApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Book Club Multiple Activity App"
+include(":app")
+ 
